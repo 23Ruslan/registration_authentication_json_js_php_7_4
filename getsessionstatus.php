abd830907de8560894c4aa78931a17d61912b4af
23ruslan <52360995+23Ruslan@users.noreply.github.com>
2022-07-23 15:14:33 +0300

    php7_4

--- getsessionstatus.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // create PHPSESSID cookies on the client side
-$_SESSION['step2'] = time(); // access to php db config files (login.php/registration.php) is available only to browsers where the button is pressed for 2 seconds (view js file)
-if (isset($_SESSION["name"]) && !empty($_SESSION["name"])) // but here is just the session status checking when the user navigates between the pages of the website
-file_put_contents('php://output', json_encode(["msg" => $_SESSION["name"]]));
-else file_put_contents('php://output', json_encode(["msg" => "no_session"]));
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start(); // create PHPSESSID cookies on the client side
-if(isset($_SESSION["name"]) && !empty($_SESSION["name"]))
-    unset($_SESSION["name"]);
-file_put_contents('php://output', json_encode(["msg"=> "no_session"]));
