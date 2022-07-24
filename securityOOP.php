176a1877f445395cae54a310609f2c3bb221943a
23ruslan <52360995+23Ruslan@users.noreply.github.com>
2022-07-24 22:33:48 +0300

    small fix after testing with weak smartphone
    	In a weak smartphone, where even the response from the screen tap takes 2 seconds, the ajax request did not have time to arrive during this time, so 2 additional seconds were added for the "secure session".
    	In a good way, in this way of protection, it would be better to generally associate the time of the "secure session" with the response time of the ajax request (I did this, for example, with blocking a button while waiting for an ajax response), and not "hard-code" this time, because there are devices with very different performance, but I did not add this, because this is just a registration prototype of the "secure session".

--- securityOOP.php
+++ securityOOP.php
@@ -3,10 +3,10 @@ if (isset($_SESSION['step1']) && password_verify("-g32gpm32g-?edm`~,pfw", $_SESS
 {
 } // password_ is just function, not password hash
 else die(" ACCESS DENIED! ERROR 404"); // basic revoking access to php db config files (login.php/registration.php) by checking if the browser was on index.php
-if (isset($_SESSION['step2']) && $_SESSION['step2'] + 3 >= time())
+if (isset($_SESSION['step2']) && $_SESSION['step2'] + 5 >= time())
 {
 }
-else die(" ACCESS DENIED! ERROR 404"); // access to php db config files (login.php/registration.php) is available only to browsers where the button is pressed for 3 seconds (view script.js)
+else die(" ACCESS DENIED! ERROR 404"); // access to php db config files (login.php/registration.php) is available only to browsers where the button is pressed for 5 seconds (view script.js)
 interface whatIsSafety
 {
     static function cleardata($text);
