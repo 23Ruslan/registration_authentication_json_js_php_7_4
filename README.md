f0de434d15cff342ad15ac7a1af149b55bb08ccf
23ruslan <52360995+23Ruslan@users.noreply.github.com>
2022-07-28 09:53:45 +0300

    some PSR-12 auto-formatting; folder splitting

--- README.md
+++
@@ -1,4 +0,0 @@
-***
-### _I did it on a local XAMPP server and transferred it to a free_ 🌐 _hosting:_ https://reg.link2site.site/
-### 
-***
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
    session_start();
-   $_SESSION["step1"] = "-g32gpm32g-?edm`~,pfw"; // revoking access to php db config files (login.php/registration.php) by checking if the browser was on index.php
-   require_once "header.php"; // instead of duplication header code ?>
+   // revoking access to php db config files (login.php/registration.php) by checking if the browser was on index.php :
+   $_SESSION["step1"] = "-g32gpm32g-?edm`~,pfw"; 
+   require_once 'layout' . DIRECTORY_SEPARATOR . 'header.php'; // instead of duplication header code ?>
 <head>
    <title>Main Page</title>
 </head>
@@ -22,13 +23,13 @@
                         name,<br>
                      </td>
                      <td id="td50r">
-                        <input type="text" required placeholder="Enter your login" id="login" form="regform" minlength="6" autofocus="autofocus"><br>
-                        <input type="password" required placeholder="Enter your password" id="password" form="regform" minlength="6" pattern="^.*(?=.*\d.*\d)(?=.*[a-zA-Z].*[a-zA-Z]).*$" oninput='checkpasswordconfirmation();'><br>
-                        <input type="password" required placeholder="Confirm your password" id="confirm_password" form="regform" minlength="6" pattern="^[a-zA-Z0-9]+$" oninput='checkpasswordconfirmation();'><br>
-                        <input type="email" required placeholder="Enter your email" id="email" form="regform"><br>
-                        <input type="text" required placeholder="Enter your full name" id="name" form="regform" minlength="2" pattern="^[a-zA-Z]+$"><br>
+                        <input type="text" required placeholder="Enter your login" id="login" form="regform" minlength="6" maxlength="200" autofocus="autofocus"><br>
+                        <input type="password" required placeholder="Enter your password" id="password" form="regform" minlength="6" maxlength="200" pattern="^.*(?=.*\d.*\d)(?=.*[a-zA-Z].*[a-zA-Z]).*$" oninput='checkpasswordconfirmation();'><br>
+                        <input type="password" required placeholder="Confirm your password" id="confirm_password" form="regform" minlength="6" maxlength="200" pattern="^[a-zA-Z0-9]+$" oninput='checkpasswordconfirmation();'><br>
+                        <input type="email" required placeholder="Enter your email" id="email" maxlength="200" form="regform"><br>
+                        <input type="text" required placeholder="Enter your full name" id="name" form="regform" minlength="2" maxlength="200" pattern="^[a-zA-Z]+$"><br>
                      </td>
-                     <span id="hint">hint: the registration fields will be fully validated only after the button is clicked. password - at least 6 characters, must include numbers and letters</span>
+                     <span id="hint"> hint: the registration fields will be fully validated only after the button is clicked. password - at least 6 characters, must include numbers and letters... any field has a maximum of 200 characters </span>
                      <td id="50l">
                         <span id="loginMessage"></span><br>
                         <span id="passwordMessage"></span><br>
@@ -48,8 +49,8 @@
                <table id="table100">
                   <tr>
                      <td id="td50r">
-                        <input type="text" required placeholder="Enter login" id="login1" form="logform" minlength="6"><br>
-                        <input type="password" required placeholder="Enter password" id="password1" form="logform" minlength="6" pattern="^.*(?=.*\d.*\d)(?=.*[a-zA-Z].*[a-zA-Z]).*$"><br>
+                        <input type="text" required placeholder="Enter login" id="login1" form="logform" minlength="6" maxlength="200"><br>
+                        <input type="password" required placeholder="Enter password" id="password1" form="logform" minlength="6" maxlength="200" pattern="^.*(?=.*\d.*\d)(?=.*[a-zA-Z].*[a-zA-Z]).*$"><br>
                         <label><input type="checkbox" class="password-checkbox">show password</label>
                      </td>
                      <td id="td50l">
@@ -63,4 +64,4 @@
          </td>
       </tr>
    </table>
-   <?php require_once "footer.php"; // instead of duplication footer code ?>
+   <?php require_once 'layout' . DIRECTORY_SEPARATOR . 'footer.php'; // instead of duplication footer code
